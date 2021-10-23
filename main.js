2a729a1ba9a27e3f68e540555a0ec5ecc6c19712
Leandro <lazarteleandro5@gmail.com>
2021-10-23 10:16:52 -0300

    avances menores

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const bird = document.getElementById('bird');
 const pipeline = document.getElementById("pipeline")
 const topPipe = document.getElementById("topPipe")
 const bottomPipe = document.getElementById("bottomPipe");
-const container=document.getElementById("gaemContainer");
+const container = document.getElementById("gameContainer");
 
 
 /*
@@ -16,92 +16,93 @@ const container=document.getElementById("gaemContainer");
 *para empujar hacias arriba.
 */
 // console.log(bottomPipe.getBoundingClientRect());
-function movimiento(event){
-    let widthBird=bird.getBoundingClientRect().width;
-    let heightBird=bird.getBoundingClientRect().height;
-    let birdX=bird.getBoundingClientRect().x+ widthBird;
-    let birdY=bird.getBoundingClientRect().y+ heightBird;
-    let bottomPipeX=bottomPipe.getBoundingClientRect().x;
-    let bottomPipeY=bottomPipe.getBoundingClientRect().y;
-    let topPipeX=topPipe.getBoundingClientRect().x;
-    let topPipeY=topPipe.getBoundingClientRect().y;
-    let bottomPipeWidth=bottomPipe.getBoundingClientRect().width;
-    let bottomPipeHeight=bottomPipe.getBoundingClientRect().height;
-    let topPipeWidth=bottomPipe.getBoundingClientRect().width;
-    let topPipeHeight=bottomPipe.getBoundingClientRect().height;
-    let birdYT=bird.getBoundingClientRect().y;
-    if(birdX>=topPipeX && birdX<=topPipeX + topPipeWidth
-       && birdYT>= topPipeY && birdYT<=topPipeY+ topPipeHeight){
-         
-        bird.style.animation='up 0.95s ease-in-out ';
-        bird.style.top='500px';
-  
-// caer();
+function movimiento(event) {
+    let widthBird = bird.getBoundingClientRect().width;
+    let heightBird = bird.getBoundingClientRect().height;
+    let birdX = bird.getBoundingClientRect().x + widthBird;
+    let birdY = bird.getBoundingClientRect().y + heightBird;
+    let bottomPipeX = bottomPipe.getBoundingClientRect().x;
+    let bottomPipeY = bottomPipe.getBoundingClientRect().y;
+    let topPipeX = topPipe.getBoundingClientRect().x;
+    let topPipeY = topPipe.getBoundingClientRect().y;
+    let bottomPipeWidth = bottomPipe.getBoundingClientRect().width;
+    let bottomPipeHeight = bottomPipe.getBoundingClientRect().height;
+    let topPipeWidth = bottomPipe.getBoundingClientRect().width;
+    let topPipeHeight = bottomPipe.getBoundingClientRect().height;
+    let birdYT = bird.getBoundingClientRect().y;
+    //  }
+    // if (event.keyCode == '39') {//derecha
+
+
+    //     x = x + 10;
+    //     bird.style.left = x + 'px';
+    // }
+
+    // if (event.keyCode == '37') {//Izquierda
+    //     x = x - 10;
+    //     bird.style.left = x + 'px';
+    // }
+
+   
+
+    if (event.keyCode == '38') {//arriba
+        let birdAux = birdYT-200;
+        element.style.setProperty("myVar0", birdYT);
+        element.style.setProperty("myVar100", birdAux);
+        bird.style.animation = 'up 0.95s ease-in-out'; //ease-out-quart
+        bird.style.top = 500+'px';
+    }
+
+
+
+     
+
+
+    if (birdX >= topPipeX && birdX <= topPipeX + topPipeWidth
+        && birdYT >= topPipeY && birdYT <= topPipeY + topPipeHeight) {
+
+        bird.style.animation = 'up 0.95s ease-in-out ';
+        bird.style.top = '500px';
+
+        // caer();
 
     }
+
     //chequeada
-    if(birdX>=bottomPipeX && birdX<=bottomPipeX + bottomPipeWidth
-        && birdY>= bottomPipeY && birdY<=bottomPipeY+ bottomPipeHeight){
-         console.log("cae");
-     }
-    // console.log(bird.getBoundingClientRect());
-    // console.log(bottomPipe.getBoundingClientRect());
-    // let topBird=bird.getBoundingClientRect().top;
-    // let bottomBird=bird.getBoundingClientRect().bottom;
-    // let rightBird=bird.getBoundingClientRect().right;
-    // let pipeTop=topPipe.getBoundingClientRect().bottom;
-    // let pipeBottom=bottomPipe.getBoundingClientRect().top-100;
-    // let topPipeLeft=topPipe.getBoundingClientRect().left+10;
-    // let bottomPipeLeft=bottomPipe.getBoundingClientRect().left+10;
-    //  if(topBird>=pipeTop || bottomBird>=pipeBottom|| rightBird>=topPipeLeft || rightBird>=bottomPipeLeft){
-    //      console.log("cae");
-    
-    //  }
-	if(event.keyCode == '39'){//derecha
-        
-       
-		x= x+10;
-		bird.style.left = x + 'px';
-	}
+    if (birdX >= bottomPipeX && birdX <= bottomPipeX + bottomPipeWidth
+        && birdY >= bottomPipeY && birdY <= bottomPipeY + bottomPipeHeight) {
+        console.log("cae");
+    }
 
-	if(event.keyCode == '37'){//Izquierda
-		x= x - 10;
-		bird.style.left = x +'px';	
-	}
-	
-	if(event.keyCode == '38'){//arriba
-		y = y + 10;                               
-		bird.style.top = (-y) + 'px';
-   
-	}
+    // if (event.keyCode == '40') {//abajo
+    //     y = y - 10;
+    //     bird.style.top = (-y) + 'px';
+    // }
 
-	if(event.keyCode == '40'){//abajo
-		y = y - 10;                            
-		bird.style.top = (-y) + 'px';
-	}
-     
+}
+
+
+function caer() {
+
+    bird.style.animation = 'down 1.1s ease-in-out ';
+    bird.style.top = '500px';
+
 }
 
 
-function caer(){
+// function mantener() {
 
+// }
 
-	
-}
+window.onkeyup = caer;
+window.onkeydown = movimiento;
 
+function pierde() {
 
-function mantener(){
-
-}
-window.onkeyup = movimiento;
-window.onkeydown=mantener;
-
-function pierde(){
- 
     // console.log(bird.getBoundingClientRect());
     // console.log(topPipe.offsetLeft);
     const tocaIzq = bird.offsetLeft >= topPipe.offsetLeft - bird.clientWidth;
-  
+
     // const offsetRightPipe = (pipeline.offsetLeft + 2 * bird.clientWidth);
 
     // const behind = offsetRightPipe < bird.offsetLeft;
@@ -115,8 +116,8 @@ function pierde(){
     // const touchingBotton = bird.offsetTop <= bottomPipe.offsetHeight;
     // console.log("perdio");
     // return (touchingTop || touchingBotton);
-    
-   
+
+
 }
 
 pierde();
@@ -136,7 +137,7 @@ pierde();
 //     for(let i=200; i<=700; i+=200){
 //        bottomPipe.style.marginLeft= `${i}px`;
 //        topPipe.style.marginLeft= `${i}px`;
-     
+
 //     }
         // bottomPipe.style.marginLeft= `${index}px`;
         // topPipe.style.marginLeft= `${i}px`;
