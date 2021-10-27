b031763f849ca884a78ddf6300ef3b7c86afa9dc
Rocio Poppe <rociocpoppe@gmail.com>
2021-10-27 10:00:08 -0300

    agrego monedas

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const pipeline = document.getElementById("pipeline")
 const topPipe = document.getElementById("topPipe")
 const bottomPipe = document.getElementById("bottomPipe");
 const container=document.getElementById("gameContainer");
-
 let birdLeft = 10;
 let birdBottom = 100;
 let gravity = 3;
@@ -22,89 +21,6 @@ let isGameOver=false;
         bird.style.left = birdLeft + 'px';
     }
     let gameTimerId = setInterval(startGame, 20)
-/*
-*Al llevar el eje de coordenadas al documento html hacemos una traducción
-*debido a que funcionan distinto y esa diferencia es más evidente cuando trabajamos en el top.
-*
-*En el eje de coordenadas incremetamos para mover hacia arriba, en el top decrementamos
-*para empujar hacias arriba.
-*/
-// console.log(bottomPipe.getBoundingClientRect());
-// function movimiento(event) {
-//     let widthBird = bird.getBoundingClientRect().width;
-//     let heightBird = bird.getBoundingClientRect().height;
-//     let birdX = bird.getBoundingClientRect().x + widthBird;
-//     let birdY = bird.getBoundingClientRect().y + heightBird;
-//     let bottomPipeX = bottomPipe.getBoundingClientRect().x;
-//     let bottomPipeY = bottomPipe.getBoundingClientRect().y;
-//     let topPipeX = topPipe.getBoundingClientRect().x;
-//     let topPipeY = topPipe.getBoundingClientRect().y;
-//     let bottomPipeWidth = bottomPipe.getBoundingClientRect().width;
-//     let bottomPipeHeight = bottomPipe.getBoundingClientRect().height;
-//     let topPipeWidth = bottomPipe.getBoundingClientRect().width;
-//     let topPipeHeight = bottomPipe.getBoundingClientRect().height;
-//     let birdYT = bird.getBoundingClientRect().y;
-//     //  }
-//     // if (event.keyCode == '39') {//derecha
-
-
-//     //     x = x + 10;
-//     //     bird.style.left = x + 'px';
-//     // }
-
-//     // if (event.keyCode == '37') {//Izquierda
-//     //     x = x - 10;
-//     //     bird.style.left = x + 'px';
-//     // }
-
-   
-
-//     // if (event.keyCode == '38') {//arriba
-//     //     let birdAux = birdYT-200;
-//     //     element.style.setProperty("myVar0", birdYT);
-//     //     element.style.setProperty("myVar100", birdAux);
-//     //     bird.style.animation = 'up 0.95s ease-in-out'; //ease-out-quart
-//     //     bird.style.top = 500+'px';
-//     // }
-
-
-
-     
-
-
-//     if (birdX >= topPipeX && birdX <= topPipeX + topPipeWidth
-//         && birdYT >= topPipeY && birdYT <= topPipeY + topPipeHeight) {
-
-
-//         bird.style.animation = 'up 0.95s ease-in-out ';
-//         bird.style.top = '500px';
-
-//         // caer();
-
-//     }
-
-//     //chequeada
-//     if (birdX >= bottomPipeX && birdX <= bottomPipeX + bottomPipeWidth
-//         && birdY >= bottomPipeY && birdY <= bottomPipeY + bottomPipeHeight) {
-//         console.log("cae");
-//     }
-
-//     // if (event.keyCode == '40') {//abajo
-//     //     y = y - 10;
-//     //     bird.style.top = (-y) + 'px';
-//     // }
-
-	
-// }
-
-
-// function caer() {
-
-//     bird.style.animation = 'down 1.1s ease-in-out ';
-//     bird.style.top = '500px';
-
-// }
-
 
 
 
@@ -123,14 +39,16 @@ let debug = "";
 
 function generateObstacle() {
 
-    console.log(debug);
-    let obstacleLeft=container.getBoundingClientRect().width;;
+    let obstacleLeft=1100//--> se empiezan a dibujar afuera por esto ->container.getBoundingClientRect().width;
+    console.log(obstacleLeft);
     let bottomPipe = document.createElement('div');
     let topPipe = document.createElement('div');
+    let coin=document.createElement('div');
     if (isGameOver==false) {
         console.log("gameover " +isGameOver);
         bottomPipe.classList.add('bottomPipe');
         topPipe.classList.add('topPipe');
+        coin.classList.add('coin');
     }
     let heightTpipe = Math.floor(Math.random() * ((container.getBoundingClientRect().height/2 - 100)-50) + 50);//170
 
@@ -143,19 +61,20 @@ function generateObstacle() {
         heightBpipe = 299;
         bottomPipe.style.height = `${heightBpipe}px`;
     }
-    
     let marginBottomPipe=container.getBoundingClientRect().height-heightBpipe;
     bottomPipe.style.marginTop=`${marginBottomPipe}px`;
     pipeline.appendChild(topPipe);
     pipeline.appendChild(bottomPipe);
+    pipeline.appendChild(coin);
     bottomPipe.style.width='60px';  
     topPipe.style.width='60px';
-
+    coin.style.top='200px';
     let timerId = setInterval(moveObstacle, 20);
     function moveObstacle() {
         obstacleLeft -=2;
         bottomPipe.style.left = obstacleLeft + 'px';
         topPipe.style.left = obstacleLeft + 'px';
+        coin.style.left=obstacleLeft + 'px';
         let widthBird = bird.getBoundingClientRect().width;
         let heightBird = bird.getBoundingClientRect().height;
         let birdX = bird.getBoundingClientRect().x + widthBird;
@@ -170,11 +89,14 @@ function generateObstacle() {
         let topPipeHeight = topPipe.getBoundingClientRect().height;
         let birdYT = bird.getBoundingClientRect().y;
         
-        if (obstacleLeft < -130 ) {
-
+        if (obstacleLeft === 100 ) {
+            //si pones <-130, las pipes desaparecen fuera de la pantalla, fijate que con esto desaparecen antes de 
+            //llegar al borde izquierdo
+            console.log("entra");
             clearInterval(timerId);
             pipeline.removeChild(bottomPipe);
             pipeline.removeChild(topPipe);
+            pipeline.removeChild(coin);
         }
 
         if (birdBottom<container.getBoundingClientRect().height*(-1)+heightBird || 
